161d20f249ba6e2e0571fa3e00b7eca07132d06d
Namith L <namithnamith370@gmail.com>
2025-08-01 14:53:53 +0530

    Frontend build changes and config updates

--- backend/src/main/java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import java.util.Optional;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import model.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email);
-}
--- backend/src/main/java/repository/WorkerRepository.java
+++
@@ -1,6 +0,0 @@
-package repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import model.Worker;
-
-public interface WorkerRepository extends MongoRepository<Worker, String> {}
